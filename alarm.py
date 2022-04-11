db8ed92234a43849497a6c93a87caaa6a0a97441
Nektatios Papagalakis <nektarios00007@gmail.com>
2022-04-11 11:00:00 +0300

    r

---
+++ alarm.py
@@ -0,0 +1,25 @@
+import winsound
+
+winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+
+'SystemAsterisk'
+	
+
+Asterisk
+
+'SystemExclamation'
+	
+
+Exclamation
+
+'SystemExit'
+	
+
+Exit Windows
+
+'SystemHand'
+	
+
+Critical Stop
+
+'SystemQuestion'
